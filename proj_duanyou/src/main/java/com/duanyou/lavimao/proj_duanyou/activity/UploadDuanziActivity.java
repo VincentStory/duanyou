a9ea3360c58008b0c92c2d0591efb489f34173be
luojialun <623572967@qq.com>
2018-05-02 22:09:03 +0800

    增加上传段子页面

--- proj_duanyou/src/main/java/com/duanyou/lavimao/proj_duanyou/activity/UploadDuanziActivity.java
+++ proj_duanyou/src/main/java/com/duanyou/lavimao/proj_duanyou/activity/UploadDuanziActivity.java
@@ -1,45 +1,53 @@
 package com.duanyou.lavimao.proj_duanyou.activity;
 
 import android.view.View;
+import android.widget.TextView;
 
 import com.duanyou.lavimao.proj_duanyou.R;
 import com.duanyou.lavimao.proj_duanyou.base.BaseActivity;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MoreActivity extends BaseActivity {
+public class UploadDuanziActivity extends BaseActivity {
+
+    @BindView(R.id.nav_title)
+    TextView titleTv;
+    @BindView(R.id.right_tv)
+    TextView rightTv;
 
     @Override
     public void setView() {
-        setContentView(R.layout.activity_more);
+        setContentView(R.layout.activity_upload_duanzi);
         ButterKnife.bind(this);
     }
 
     @Override
     public void initData() {
-
+        initTitle();
     }
 
+    private void initTitle() {
+        titleTv.setText("段子");
+        rightTv.setText("发布");
+    }
 
     @Override
     public void startInvoke() {
 
     }
 
-    @OnClick({R.id.close_iv, R.id.duanzi_ll, R.id.duanyouxiu_ll})
+    @OnClick({R.id.iv_left})
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.close_iv:
+            case R.id.iv_left:
                 finish();
                 break;
-            case R.id.duanzi_ll:
-                jumpTo(UploadDuanziActivity.class);
-                finish();
+            case R.id.right_tv:
+                //发布操作
                 break;
-            case R.id.duanyouxiu_ll:
-                break;
-
         }
     }
+
 }
