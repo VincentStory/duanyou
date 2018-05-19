96f432c29a8e99ceebdcbce396e8edfce17d7b2f
luojialun <623572967@qq.com>
2018-05-19 23:48:23 +0800

    下载图片方法

--- esbsdk/src/main/java/com/xiben/ebs/esbsdk/esb/BaseClientProxy.java
+++ esbsdk/src/main/java/com/xiben/ebs/esbsdk/esb/BaseClientProxy.java
@@ -1,6 +1,8 @@
 package com.xiben.ebs.esbsdk.esb;
 
 import android.annotation.SuppressLint;
+import android.os.Handler;
+import android.os.Message;
 import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
@@ -10,7 +12,9 @@ import com.xiben.ebs.esbsdk.callback.InvokeCallback;
 import com.xiben.ebs.esbsdk.util.LogUtil;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.SecureRandom;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
@@ -239,6 +243,131 @@ public class BaseClientProxy {
         }
 
     }
+
+    public static final int DOWNLOAD_FAIL = 0;
+    public static final int DOWNLOAD_PROGRESS = 1;
+    public static final int DOWNLOAD_SUCCESS = 2;
+
+    public  void download(final String url, final String saveDir, final OnDownloadListener listener) {
+        this.listener = listener;
+        Request request = new Request.Builder().url(url).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Message message = Message.obtain();
+                message.what = DOWNLOAD_FAIL;
+                mHandler.sendMessage(message);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                InputStream is = null;
+                byte[] buf = new byte[2048];
+                int len = 0;
+                FileOutputStream fos = null;
+                //储存下载文件的目录
+                String savePath = saveDir;
+                try {
+                    is = response.body().byteStream();
+                    long total = response.body().contentLength();
+                    File file = new File(savePath);
+                    fos = new FileOutputStream(file);
+                    long sum = 0;
+                    while ((len = is.read(buf)) != -1) {
+                        fos.write(buf, 0, len);
+                        sum += len;
+                        int progress = (int) (sum * 1.0f / total * 100);
+                        //下载中
+                        Message message = Message.obtain();
+                        message.what = DOWNLOAD_PROGRESS;
+                        message.obj = progress;
+                        mHandler.sendMessage(message);
+
+                    }
+                    fos.flush();
+                    //下载完成
+                    Message message = Message.obtain();
+                    message.what = DOWNLOAD_SUCCESS;
+                    message.obj = file.getAbsolutePath();
+                    mHandler.sendMessage(message);
+                } catch (Exception e) {
+                    Message message = Message.obtain();
+                    message.what = DOWNLOAD_FAIL;
+                    mHandler.sendMessage(message);
+                } finally {
+                    try {
+                        if (is != null)
+                            is.close();
+                    } catch (IOException e) {
+
+                    }
+                    try {
+                        if (fos != null) {
+                            fos.close();
+                        }
+                    } catch (IOException e) {
+
+                    }
+                }
+            }
+        });
+    }
+
+    private String getNameFromUrl(String url) {
+        return url.substring(url.lastIndexOf("/") + 1);
+    }
+
+
+    private String isExistDir(String saveDir) throws IOException {
+        File downloadFile = new File(saveDir);
+        if (!downloadFile.mkdirs()) {
+            downloadFile.createNewFile();
+        }
+        String savePath = downloadFile.getAbsolutePath();
+        return savePath;
+    }
+
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case DOWNLOAD_PROGRESS:
+                    listener.onDownloading((Integer) msg.obj);
+                    break;
+                case DOWNLOAD_FAIL:
+                    listener.onDownloadFailed();
+                    break;
+                case DOWNLOAD_SUCCESS:
+                    listener.onDownloadSuccess((String) msg.obj);
+                    break;
+            }
+        }
+    };
+
+
+    OnDownloadListener listener;
+
+    public interface OnDownloadListener {
+        /**
+         * 下载成功
+         */
+        void onDownloadSuccess(String path);
+
+        /**
+         * 下载进度
+         *
+         * @param progress
+         */
+        void onDownloading(int progress);
+
+        /**
+         * 下载失败
+         */
+        void onDownloadFailed();
+    }
+
 
     /**
      * 默认信任所有的证书 *
